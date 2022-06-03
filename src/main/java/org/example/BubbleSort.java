3ec18ebc0c7c01c1df9504160e51e9d9e5a67ae1
Zajcw <exaner9@gmail.com>
2022-06-03 11:09:25 +0200

    Added few comments in BubbleSort.java. Finished writing tests in BalancedWordsCounterTest.java.

--- src/main/java/org/example/BubbleSort.java
+++ src/main/java/org/example/BubbleSort.java
@@ -5,9 +5,14 @@ import java.util.Objects;
 
 public class BubbleSort{
     public static <T extends Comparable<T>> List<T> sort(List<T> input) throws RuntimeException {
+        //Check if input is null. Throw RuntimeException accordingly
         if(Objects.isNull(input))
             throw new RuntimeException("Input is null");
+
+        //Remove objects from list which have null as their value
         input.removeIf(Objects::isNull);
+
+        //Execute sorting using compareTo() method
         for(int i = 0; i < input.size() - 1; i++){
             for(int j = 0; j < input.size() - i - 1; j++){
                 T a =  input.get(j);
--- src/test/java/org/example/BalancedWordsCounterTest.java
+++ src/test/java/org/example/BalancedWordsCounterTest.java
@@ -7,6 +7,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedWordsCounterTest {
 
     @Test
-    void count() {
+    void should_return_28_if_input_string_is_aabbabcccba() {
+        String input = "aabbabcccba";
+        Integer expectedOutputNumber = 28;
+        Integer outputNumber = BalancedWordsCounter.count(input);
+        assertEquals(expectedOutputNumber,outputNumber);
+    }
+
+    @Test
+    void should_return_0_if_input_string_is_empty(){
+        String input = "";
+        Integer expectedOutputNumber = 0;
+        Integer outputNumber = BalancedWordsCounter.count(input);
+        assertEquals(expectedOutputNumber,outputNumber);
+    }
+
+    @Test
+    void should_throw_runtime_exception_if_input_string_contains_non_alphabet_character(){
+        String input = "abababa1";
+        assertThrows(RuntimeException.class, () -> {BalancedWordsCounter.count(input);});
+    }
+
+    @Test
+    void should_throw_runtime_exception_if_input_is_null(){
+        String input = null;
+        assertThrows(RuntimeException.class, () -> {BalancedWordsCounter.count(input);});
     }
 }
